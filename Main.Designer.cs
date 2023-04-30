262071c1a6af6c833d36b4a2def6bedf81308137
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2023-04-30 12:37:13 -0400

    Cambios de seguridad

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -29,20 +29,35 @@ namespace Glowish_Fashion_System
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SF Pro Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(109, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hola amigos";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(570, 263);
+            this.Controls.Add(this.label1);
             this.Name = "Main";
             this.Text = "Main";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
