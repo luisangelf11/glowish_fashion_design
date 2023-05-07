d9c7a7ac5f06c120f044bab030e5062f8ebb0318
Breiddy <breiddysubzero@gmail.com>
2023-05-07 16:46:55 -0400

    Todo listo, tuve que hacer otro commit. Fok

---
+++ Notifications/Loader.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Glowish_Fashion_System.Notifications
+{
+    partial class Loader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Loader));
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.btnBienvenido = new Guna.UI2.WinForms.Guna2Button();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 30;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
+            this.guna2PictureBox1.ImageFlip = Guna.UI2.WinForms.Enums.FlipOrientation.Horizontal;
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(538, 258);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.guna2PictureBox1.TabIndex = 33;
+            this.guna2PictureBox1.TabStop = false;
+            this.guna2PictureBox1.Click += new System.EventHandler(this.guna2PictureBox1_Click);
+            // 
+            // btnBienvenido
+            // 
+            this.btnBienvenido.BackColor = System.Drawing.Color.Transparent;
+            this.btnBienvenido.BorderRadius = 9;
+            this.btnBienvenido.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnBienvenido.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnBienvenido.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnBienvenido.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnBienvenido.FillColor = System.Drawing.Color.Transparent;
+            this.btnBienvenido.Font = new System.Drawing.Font("SF Pro Display", 24F, System.Drawing.FontStyle.Bold);
+            this.btnBienvenido.ForeColor = System.Drawing.Color.White;
+            this.btnBienvenido.HoverState.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.btnBienvenido.HoverState.CustomBorderColor = System.Drawing.Color.WhiteSmoke;
+            this.btnBienvenido.HoverState.FillColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnBienvenido.Location = new System.Drawing.Point(167, 92);
+            this.btnBienvenido.Name = "btnBienvenido";
+            this.btnBienvenido.Size = new System.Drawing.Size(199, 65);
+            this.btnBienvenido.TabIndex = 67;
+            this.btnBienvenido.Text = "Bienvenido";
+            this.btnBienvenido.UseTransparentBackground = true;
+            // 
+            // Loader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(538, 258);
+            this.Controls.Add(this.btnBienvenido);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Loader";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loader";
+            this.Load += new System.EventHandler(this.Loader_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2Button btnBienvenido;
+    }
+}
---
+++ Notifications/Loader.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Glowish_Fashion_System.Notifications
+{
+    public partial class Loader : Form
+    {
+        public Loader()
+        {
+            InitializeComponent();
+           
+            
+
+        }
+        
+        int r = 255;
+        int g = 255;
+        int b = 255;
+        int step = 5;
+        
+        private void Loader_Load(object sender, EventArgs e)
+        {
+            btnBienvenido.ForeColor = Color.FromArgb(r,g, b);
+            timer1.Interval = 50; 
+
+            timer1.Start();
+            this.Opacity = 0;
+            timer2.Start();
+
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            r = Math.Max(0, r - step);
+            g = Math.Max(0, g - step);
+            b = Math.Max(0, b - step);
+
+            // actualiza el color de fondo del formulario
+            btnBienvenido.ForeColor = Color.FromArgb(r, g, b);
+
+            // detiene el temporizador si se ha alcanzado el negro
+            if (r == 0 && g == 0 && b == 0)
+            {
+                timer1.Stop();
+                this.Close();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (this.Opacity < 1) this.Opacity += 0.05;
+            if (this.Opacity == 1) timer2.Stop();
+        }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
