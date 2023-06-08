98211e9e6d65b73ccbbd8498f46e56cd982ea41b
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2023-06-07 23:01:32 -0400

    Reporte Clientes Semi-Diseñado

---
+++ Reports/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glowish_Fashion_System.Reports
+{
+    public class Cliente
+    {   
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public int Edad { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+    }
+}
